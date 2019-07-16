6d39286484c9048ee5682825e430ac74569e7222
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-07-16 18:35:15 +0800

    Add just flask linter

--- flask/app.py
+++ flask/app.py
@@ -3,9 +3,11 @@ import pika
 
 app = Flask(__name__)
 
+
 @app.route('/api/produce/get', methods=['GET'])
 def index():
     return "Test"
+
 
 @app.route('/api/produce/message', methods=['POST'])
 def incoming():
@@ -20,6 +22,7 @@ def incoming():
                           routing_key='hello',
                           body=data)
     return Response('Data sent was: {}'.format(data), status=200)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
