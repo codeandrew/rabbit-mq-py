675c5b1ac3f1cec4267ea8a46af8f10617ed1132
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-07-17 19:10:40 +0800

    Pep8 Linter

--- app/producer/views.py
+++ app/producer/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, permissions
 from .models import Items
 from .serializers import ItemSerializer
 
+
 class ItemView(viewsets.ModelViewSet):
     queryset = Items.objects.all()
     serializer_class = ItemSerializer
-    
